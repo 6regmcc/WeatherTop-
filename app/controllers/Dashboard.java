96aa046384140b382b4b03087213c5d30a1e00a1
Greg <6regmcc@gmail.com>
2022-05-27 22:08:54 +0100

    added delete station

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -52,6 +52,18 @@ public class Dashboard extends Controller
     redirect ("/dashboard");
   }
 
+  public static void deleteStation (Long id)
+  {
+
+    Member member = Accounts.getLoggedInMember();
+    Station station = Station.findById(id);
+    Logger.info ("Removing" + station.name + id);
+    member.stations.remove(station);
+    member.save();
+    station.delete();
+    redirect ("/dashboard");
+  }
+
 
 
 }
