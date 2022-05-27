8be335417110511082a2c41584733da4942e600d
Greg <6regmcc@gmail.com>
2022-05-27 23:08:22 +0100

    added  edit email

---
+++ app/controllers/UpdateUser.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import models.Member;
+import models.Reading;
+import models.Station;
+import play.Logger;
+import play.mvc.Controller;
+
+public class UpdateUser extends Controller {
+    public static void index(){
+
+        Member member = Accounts.getLoggedInMember();
+        Logger.info("Rendering User");
+        render("user.html", member);
+    }
+
+    public static void updateEmail(long id, String email){
+        Member member  = Member.findById(id);
+        member.email = email;
+        Logger.info("this method was called" + id + email );
+        redirect("/user");
+    }
+}
